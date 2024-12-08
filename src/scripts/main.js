827b23d6d96748f7dfe648479191b5f5b8ab6539
nex1994 <bilicki1994@gmail.com>
2024-12-08 11:43:52 +0100

    init commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+const executor = (resolve, reject) => {
+  const message = document.createElement('div');
+
+  logo.addEventListener('click', () => {
+    message.className = 'message';
+    message.textContent = 'Promise was resolved!';
+    resolve(document.body.appendChild(message));
+  });
+
+  setTimeout(() => {
+    message.className = 'message error-message';
+    message.textContent = 'Promise was rejected!';
+    reject(document.body.appendChild(message));
+  }, 3000);
+};
+
+// eslint-disable-next-line no-unused-vars
+const promise1 = new Promise(executor);
